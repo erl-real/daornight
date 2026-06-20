3c77df8327b6668bbacd7d8231754b5445c0f032
Real Is Rare of NC <sickinknh@gmail.com>
2026-06-20 02:29:58 -0400

    Add files via upload

---
+++ maps/CityBasic.js
@@ -0,0 +1,50 @@
+export const CityBasicMap = {
+    name: "City Basic",
+    buildings: [
+        { 
+            position: { x: 50, y: 15, z: 50 }, 
+            size: { x: 15, y: 30, z: 15 },
+            color: 0x444444
+        },
+        { 
+            position: { x: -60, y: 25, z: 20 }, 
+            size: { x: 20, y: 50, z: 20 },
+            color: 0x333333
+        },
+        { 
+            position: { x: 20, y: 20, z: -70 }, 
+            size: { x: 25, y: 40, z: 25 },
+            color: 0x555555
+        }
+    ],
+    pickups: [
+        { x: 200, z: 200, type: 'health' },
+        { x: -200, z: 200, type: 'charge' },
+        { x: 0, z: -350, type: 'ammo' },
+        { x: 500, z: 500, type: 'ammo' },
+        { x: -500, z: -500, type: 'ammo' },
+        { x: 500, z: -500, type: 'charge' },
+        { x: -500, z: 500, type: 'charge' },
+        { x: 0, z: 250, type: 'buff_hover' },
+        { x: 150, z: -150, type: 'ult' },
+        { x: -150, z: -150, type: 'missile' },
+        { x: 250, z: 150, type: 'mortar' },
+        { x: -250, z: 150, type: 'shotgun' },
+        { x: 150, z: 250, type: 'turret' },
+        { x: -150, z: 250, type: 'cannon' },
+        { x: 250, z: -150, type: 'melee' },
+        { x: -250, z: -150, type: 'energywep' }
+    ],
+    barrels: [
+        { x: 150, z: 150, type: 'explosive' },
+        { x: -150, z: -150, type: 'explosive' },
+        { x: 300, z: -250, type: 'explosive' },
+        { x: -350, z: 400, type: 'cryo' },
+        { x: 600, z: 0, type: 'cryo' },
+        { x: 0, z: 600, type: 'toxic' },
+        { x: -600, z: -400, type: 'toxic' },
+        { x: 250, z: 450, type: 'oil' },
+        { x: -450, z: 250, type: 'oil' }
+    ],
+    spawnAI: false
+};
---
+++ maps/DevMap.js
@@ -0,0 +1,35 @@
+export const DevMap = {
+    name: "Development Lab",
+    buildings: [],
+    ramps: [
+        { x: 40, y: 0.5, z: 40, w: 10, h: 1.5, d: 10, rotX: 0.25 },
+        { x: 60, y: 1.2, z: 40, w: 12, h: 3, d: 14, rotX: 0.25 },
+        { x: 85, y: 2.5, z: 40, w: 15, h: 6, d: 20, rotX: 0.25 }
+    ],
+    pickups: [
+        { x: 80, z: 80, type: 'health' },
+        { x: -80, z: 80, type: 'c4' },
+        { x: 80, z: -80, type: 'charge' },
+        { x: -80, z: -80, type: 'charge' },
+        { x: 0, z: 150, type: 'ammo' },
+        { x: 0, z: -150, type: 'ammo' },
+        { x: 25, z: 25, type: 'ult' },
+        { x: -30, z: 30, type: 'missile' },
+        { x: -20, z: 30, type: 'mortar' },
+        { x: -10, z: 30, type: 'shotgun' },
+        { x: 0, z: 30, type: 'turret' },
+        { x: 10, z: 30, type: 'cannon' },
+        { x: 20, z: 30, type: 'energywep' },
+        { x: 30, z: 30, type: 'melee' },
+        { x: 0, z: 60, type: 'buff_hover' }
+    ],
+    barrels: [
+        { x: 10, z: 10, type: 'explosive' },
+        { x: -10, z: 10, type: 'explosive' },
+        { x: 10, z: -10, type: 'cryo' },
+        { x: -10, z: -10, type: 'cryo' },
+        { x: 25, z: 0, type: 'toxic' },
+        { x: -25, z: 0, type: 'oil' }
+    ],
+    spawnAI: true
+};
